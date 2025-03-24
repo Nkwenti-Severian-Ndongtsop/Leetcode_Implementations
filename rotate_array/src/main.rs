b23367f5ea34752ec430e1a9ecbff6d17182d85d
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-24 20:59:10 +0100

    chore(file): implemented rotate array

--- rotate_array/src/main.rs
+++ rotate_array/src/main.rs
@@ -1,19 +1,15 @@
+pub fn max_profit(k: i32, mut prices: Vec<i32>) -> Vec<i32> {
+    let n = prices.len();
+    let k = (k as usize) % n; 
 
-pub fn rotate(nums: &mut Vec<i32>, k: i32) {
-    let mut n = nums.len(); //  7
-    let mut result: Vec<i32> = vec![];
-
-    for _ in 1..=k {
-        let value = nums.remove(n - 1);
-        result.insert(0, value);
-        n -= 1;
-    }
-    result.append(nums);
-    println!("{:?}", result)
+    prices.reverse(); 
+    prices[..k].reverse(); 
+    prices[k..].reverse();
+    prices
 }
 
 fn main() {
-    let test: &mut Vec<i32> = &mut vec![7, 1, 2, 3, 4, 5, 6];
-    let k = 2;
-    println!("The output is {:?}", rotate(test, k));
+    let test = vec![1,2,3,4,5,6,7];
+    let result = max_profit(3, test);
+    println!("{:?}", result);
 }
