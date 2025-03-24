112118c43107bbb6e875211b8b41330926bf15b6
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-24 20:08:08 +0100

    chore(palindrome): implemented palindrome

--- longest_palindrome/src/main.rs
+++ longest_palindrome/src/main.rs
@@ -1,18 +1,42 @@
+pub fn multiply_string(num1: String, num2: String) -> String {
+    if num1 == "0" || num2 == "0" {
+        return "0".to_string();
+    }
 
-pub fn multiply_string(num1: &str, num2: &str) -> String {
-    
-    let first_value = num1.parse::<u64>().expect("Couldn't get value");
-    let second_value = num2.parse::<u64>().expect("Couldn't get value");
+    let mut result = vec![0; num1.len() + num2.len()];
+    let num1_bytes = num1.as_bytes();
+    let num2_bytes = num2.as_bytes();
 
-    let result = (first_value * second_value).to_string();
-    result
-    
+    for i in (0..num1.len()).rev() {
+        for j in (0..num2.len()).rev() {
+            let mul = (num1_bytes[i] - b'0') as usize * (num2_bytes[j] - b'0') as usize;
+            let sum = mul + result[i + j + 1];
+
+            result[i + j + 1] = sum % 10;
+            result[i + j] += sum / 10;
+        }
+    }
+
+    let mut result_str = String::new();
+    let mut leading_zero = true;
+
+    for &digit in &result {
+        if digit != 0 || !leading_zero {
+            leading_zero = false;
+            result_str.push((digit as u8 + b'0') as char);
+        }
+    }
+
+    result_str
 }
 
-
-
 fn main() {
-    
-    let values = ("123", "456");
-    println!("The product of this strings is: {}", multiply_string(values.0, values.1));
+    let values = (
+        "401716832807512840963".to_string(),
+        "167141802233061013023557397451289113296441069".to_string(),
+    );
+    println!(
+        "The product of this strings is: {}",
+        multiply_string(values.0, values.1)
+    );
 }
