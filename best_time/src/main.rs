2d9162d100c83fb523564edecc448714bace6989
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-24 19:00:49 +0100

    chore(best_time): implemented best_time

--- best_time/src/main.rs
+++ best_time/src/main.rs
@@ -5,6 +5,9 @@ fn highest_profit(prices: Vec<i32>) -> i32 {
     // [7,1,5,3,6,4]   [7,6,4,3,1]
     let mut max_profit = vec![0];
     let mut actual_length = max_profit.len();
+    if prices.len() == 2 && prices[0] < prices[1] {
+        return prices[0]
+    }
     let mut count = 0;
     for i in 0..prices.len() - 1 {
         for j in i+1..prices.len() - 1 {
@@ -13,7 +16,6 @@ fn highest_profit(prices: Vec<i32>) -> i32 {
                 max_profit.push(temp_profit);
                 actual_length = max_profit.len();
                 count += 1;
-                println!("{:?}", max_profit)
             }
             
             
