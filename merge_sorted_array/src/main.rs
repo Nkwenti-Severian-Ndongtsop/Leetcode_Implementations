bcbab959243a5d9192ef451e786ff09da9b980ec
Nkwenti-Severian-Ndongtsop <180976800+Nkwenti-Severian-Ndongtsop@users.noreply.github.com>
2025-03-26 17:10:33 +0100

    chore(file): implementation sorted

--- merge_sorted_array/src/main.rs
+++ merge_sorted_array/src/main.rs
@@ -1,18 +1,10 @@
-use core::num;
-
-
-
 pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
-        nums2.truncate(n as usize);
+    nums2.truncate(n as usize);
 
-        nums1.truncate(m as usize);
-        nums1.extend(nums2.iter());
-        nums1.sort();
-
-    }
-
-
-
+    nums1.truncate(m as usize);
+    nums1.extend(nums2.iter());
+    nums1.sort();
+}
 
 fn main() {
     println!("Hello, world!");
