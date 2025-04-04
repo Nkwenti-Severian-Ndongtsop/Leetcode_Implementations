76e8c356f4c2da1727a9349b9d5ab7df86e7e716
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-04-04 21:47:51 +0100

    chore(file): implemented is_monotonic

---
+++ is_monotonic/src/main.rs
@@ -0,0 +1,24 @@
+
+
+
+pub fn is_monotonic(nums: Vec<i32>) -> bool {
+    let mut is_increasing = true;
+
+    for i in 1..nums.len() {
+        if nums[i] <= nums[i - 1] {
+            is_increasing = false;  
+        }
+        if nums[i] > nums[i - 1] {
+            is_increasing = false;
+        }
+    }
+    is_increasingl
+}
+
+
+
+
+fn main() {
+    let nums = vec![1, 2, 2, 3];    
+    println!("{}", is_monotonic(nums));
+}
