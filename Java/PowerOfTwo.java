843f260c7e5bbd61bc5d01bf186f778c17b05eb5
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-05-14 06:54:14 +0100

    chore(file): implementation

---
+++ Java/PowerOfTwo.java
@@ -0,0 +1,12 @@
+
+public class PowerOfTwo {
+    boolean isPowerOfThree(int n) {
+
+        if ( n < 1) return false;
+
+        while ( n % 2 == 0 ) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
