08eaee27136c64d22c19ff31458cab56c6538ae8
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-05-14 06:58:15 +0100

    chore(file): implementation

---
+++ Java/ValidAnagram.java
@@ -0,0 +1,17 @@
+public class ValidAnagram {
+    
+    public boolean isAnagram(String s, String t) {
+        
+        if ( s.length() != t.length() ) {
+            return false;
+        }
+
+        for ( int i=0; i<s.length(); i++) {
+
+            if (! s.contains(String.valueOf(t.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
