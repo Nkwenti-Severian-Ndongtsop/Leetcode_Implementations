9f4d74f197c147e64eca7ee456db50a35eb6c14e
Nkwenti-Severian-Ndongtsop <halamadrid651643565@gmail.com>
2025-05-14 06:53:51 +0100

    chore(file): implementation

---
+++ Java/MultiplyStrngs.java
@@ -0,0 +1,14 @@
+
+public class MultiplyStrngs {
+
+    public String multiply(String num1, String num2) {
+     
+        int stringVaalue1 = Integer.parseInt(num2);
+        int stringValue2 = Integer.parseInt(num2);
+
+        String stringResult = String.valueOf(stringVaalue1 * stringValue2);
+
+        return stringResult; 
+    }
+    
+}
